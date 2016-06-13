cb1de49e8d75fba35157f61297d0aad519038685
Peter Cowal <pcowal@gmail.com>
2016-06-13 16:41:01 -0700

    Revert "Added charachter equivilancies"
    This reverts commit 1ef93e5a2fec4b26842f90eafe2f9fbba571a6a7.

--- tabs-simple.js
+++ tabs-simple.js
@@ -111,9 +111,6 @@ function startup() {
   editor.getSession().setTabSize(4);
   editor.commands.bindKeys({"ctrl-l":null, "ctrl-shift-r":null, "ctrl-r":null, "ctrl-t":null})
 
-  //Sets up character equivalencies for converting expressions such as !=
-  setupCharacterEquivalencies();
-
   document.getElementById('code_txt_real').style.height = (document.getElementById('stdout_txt').clientHeight
                                                               + document.getElementById('stdout_options').clientHeight) + 'px';
   document.getElementById('code_txt_real').style.width = (document.getElementById('stdout_txt').clientWidth - 10) + 'px';
